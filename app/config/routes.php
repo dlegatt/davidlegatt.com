191243796b2ca584a921c3295a624e677cc9d150
David Legatt <dlegatt@gmail.com>
2016-01-04 19:20:41 -0600

    Added default route

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,3 +4,9 @@
 use App\Provider\HelloControllerProvider;
 
 $app->mount('/hello', new HelloControllerProvider());
+
+$app->get('/', function () use ($app) {
+    return $app->redirect($app['url_generator']->generate('GET_hello_name', [
+        'name' => 'World'
+    ]));
+});
